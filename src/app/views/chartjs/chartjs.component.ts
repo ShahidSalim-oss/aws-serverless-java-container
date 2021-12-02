2250cff8bd2c74207c66efef3dd0ca140b33493d
ShahidSalim-oss <70953311+ShahidSalim-oss@users.noreply.github.com>
2021-12-03 03:30:42 +0500

    Update chartjs.component.ts

--- src/app/views/chartjs/chartjs.component.ts
+++ src/app/views/chartjs/chartjs.component.ts
@@ -11,18 +11,24 @@ export class Friend {
   }
 }
 
+
 @Component({
   templateUrl: './chartjs.component.html'
 })
 export class HomeComponent implements OnInit {
 
+  loading:boolean = true;
+  singlefriendloading:boolean=true;
+  friend: Friend;
   friends: Friend[];
   constructor(
     private httpClient: HttpClient
   ) { }
 
   ngOnInit(): void {
+    this.loading=true;
     this.getFriends();
+    this.getFriend();
   }
 
   getFriends(){
@@ -30,6 +36,17 @@ export class HomeComponent implements OnInit {
       response => {
         console.log(response);
         this.friends = response;
+        this.loading=false;
+      }
+    );
+  }
+
+  getFriend(){
+    this.httpClient.get<any>('https://bb5uqeubxi.execute-api.us-east-1.amazonaws.com/dev/pets/2a0ea1a2-43b6-4383-a746-95a12c86c8e0').subscribe(
+      response => {
+        console.log(response);
+        this.friend = response;
+        this.singlefriendloading=false;
       }
     );
   }
