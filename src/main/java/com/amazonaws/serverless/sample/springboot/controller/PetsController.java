20e35e66d698af6b25c0cd42ae2edf49fdfb6a9e
ShahidSalim-oss <70953311+ShahidSalim-oss@users.noreply.github.com>
2022-07-16 12:11:51 +0500

    Update PetsController.java

--- src/main/java/com/amazonaws/serverless/sample/springboot/controller/PetsController.java
+++ src/main/java/com/amazonaws/serverless/sample/springboot/controller/PetsController.java
@@ -76,7 +76,7 @@ public class PetsController {
 
 		Pet newPet1 = new Pet();
         newPet1.setId(UUID.randomUUID().toString());
-        newPet1.setName("GREENUPDATED");
+        newPet1.setName("GREENUPDATEDAGAIN");
         newPet1.setBreed(PetData.getRandomBreed());
         newPet1.setDateOfBirth(PetData.getRandomDoB());
         outputPets[0] = newPet1;
